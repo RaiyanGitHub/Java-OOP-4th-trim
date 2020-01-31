9de5a125ae458d2fd0355847c1facbe30d92913e
Raiyan Ahmad Saadi <49239711+RaiyanGitHub@users.noreply.github.com>
2020-01-31 19:30:39 +0600

    Create Square Class.java

---
+++ 3rd problem/Square Class.java
@@ -0,0 +1,11 @@
+public class Square extends Shape {
+    @Override
+    public void create(){
+        System.out.println("Praise to the Lord who made me fair and square!");
+        
+    }
+    @Override
+    public void erase(){
+        System.out.println("Glorious is the Lord as I breathe and as I cease");
+    }
+}
