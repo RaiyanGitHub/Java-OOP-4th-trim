aac33b4dc7bb14928ceef982f2a03661e64fcba4
Raiyan Ahmad Saadi <49239711+RaiyanGitHub@users.noreply.github.com>
2020-01-31 19:35:16 +0600

    Create Rectangles and Square.java

---
+++ 4th Problem/Rectangles and Square.java
@@ -0,0 +1,8 @@
+public class RectangleNsQuare {
+    public void area(double side){
+        System.out.println("Area of the Square: "+side*side);
+    }
+    public void area(double length, double breadth){
+        System.out.println("Area of the Rectangle: "+length*breadth+"");
+    }
+}
