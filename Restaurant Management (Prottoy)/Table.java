b4e51be3aaede5fb0aa0e4fe9af53ce90034f062
Raiyan Ahmad Saadi <49239711+RaiyanGitHub@users.noreply.github.com>
2020-03-29 23:45:29 +0600

    Update Table.java

--- Restaurant Management (Prottoy)/Table.java
+++ Restaurant Management (Prottoy)/Table.java
@@ -4,15 +4,27 @@ package resturant.management;
 
 
 public class Table {
+	private int tableId;
     private int capacity;
     private boolean free;
 
-    public Table(int capacity, boolean free) {
+    public Table(int ID,int capacity, boolean free) {
+		this.tableId = ID;
         this.capacity = capacity;
         this.free = free;
     }
 
 
+	 public int getId() {
+        return TableId;
+    }
+	public void setId(int a){
+		tableId = a;
+	}
+
+    public void setCapacity(int capacity) {
+        this.capacity = capacity;
+    }
     public int getCapacity() {
         return capacity;
     }
@@ -28,11 +40,15 @@ public class Table {
     public boolean getFree(){
     return this.free;
     }
-
+	public void toggle(){					// Sets a table free if it is occupied and occupies it if it is free.
+		boolean a = this.getFree(); 
+		this.setFree(!a);
+	}
 
     public void display()
     {
-        System.out.println("Capacity="+capacity);
+		System.out.println("Table No."+this.getId());
+        System.out.println("Capacity="+this.getCapacity());
         if(this.getFree())
 	    System.out.println("Table is available");
 	else
