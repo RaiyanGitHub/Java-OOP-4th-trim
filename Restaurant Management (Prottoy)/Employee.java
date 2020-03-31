0dbbaf0e69fd35873741f9b641058b0fb6b11a6d
Raiyan Ahmad Saadi <49239711+RaiyanGitHub@users.noreply.github.com>
2020-03-31 10:09:44 +0600

    Update Employee.java

--- Restaurant Management (Prottoy)/Employee.java
+++ Restaurant Management (Prottoy)/Employee.java
@@ -7,6 +7,16 @@ public abstract class Employee {
     private String id;
     private String name;
     private float salary;
+    private boolean onDuty;
+    private boolean sick = false;           //so that it is initially set to false
+    private boolean onLiiv = false;         //so that it is initially set to false
+    public Employee(String id, String name, float salary, boolean active) {
+        this.id = id;
+        this.name = name;
+        this.salary = salary;
+        this.onDuty = active;
+        
+    }
 
     public Employee(String id, String name, float salary) {
         this.id = id;
@@ -38,6 +48,50 @@ public abstract class Employee {
         this.salary = salary;
     }
     
+    public boolean getOnDuty(){
+        return onDuty;
+    }
+    
+    public void setOnDuty(boolean a){                   // How best should we make use of this function and active status of an employee?
+        this.onDuty = a;
+    }
+    
+    public void toggleOnDuty(){
+        if(this.getOnDuty())
+            this.setOnDuty(false);
+        else
+            this.setOnDuty(true);
+    }
+    
+    public boolean getSick(){
+        return sick;
+    }
+    
+    public void setSick(boolean a){                   // How best should we make use of this function and active status of an employee?
+        this.sick = a;
+    }
+    
+    public void toggleSickF(){
+        if(this.getSick())
+            this.setSick(false);
+        else
+            this.setSick(true);
+    }
+    
+    public boolean getLeave(){
+        return onLiiv;
+    }
+    
+    public void setLeave(boolean a){                   // How best should we make use of this function and active status of an employee?
+        this.onLiiv = a;
+    }
+    
+    public void toggleLeave(){
+        if(this.getLeave())
+            this.setLeave(false);
+        else
+            this.setLeave(true);
+    }
     
     public abstract void display();
     public abstract void hire();
